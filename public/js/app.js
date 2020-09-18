b2c84abc213ec47f32c5bb698a653f86781b26a2
Olalekan Efunkunle <efunkunleolalekan@gmail.com>
2020-09-18 10:24:56 +0100

    fixed another  pesky bug

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ form.addEventListener('submit', (e)=>{
 const address = search.value
 console.log(address)
 
-    fetch(`gir/weather?address=${address}`).then(res=> res.json().then(data=> {
+    fetch(`/weather?address=${address}`).then(res=> res.json().then(data=> {
         if(data.error){
             place.textContent = data.error
         } else{
